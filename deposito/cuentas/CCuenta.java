feb2b053cd18b26c14074ae6d2a1bcb12fa06a5e
Javier199ES <104563050+Javier199ES@users.noreply.github.com>
2022-04-28 12:20:48 +0200

    Add files via upload

---
+++ deposito/cuentas/CCuenta.java
@@ -0,0 +1,90 @@
+package cuentas;
+public class CCuenta {
+/**
+ * Atributos
+ */
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInteres;
+/**
+ * GETTER and SETTER
+ * @return
+ */
+    public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public double getTipoInteres() {
+		return tipoInteres;
+	}
+
+	public void setTipoInteres(double tipoInteres) {
+		this.tipoInteres = tipoInteres;
+	}
+/**
+ * Constructor
+ */
+	public CCuenta()
+    {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+    }
+/**
+ * Devuelve el saldo de la cuenta
+ * @return
+ */
+    public double estado()
+    {
+        return saldo;
+    }
+/**
+ * Ingresa en la cuenta
+ * @param cantidad
+ * @throws Exception
+ */
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        saldo = saldo + cantidad;
+    }
+/**
+ * Retirar fondos
+ * @param cantidad
+ * @throws Exception
+ */
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        saldo = saldo - cantidad;
+    }
+}
